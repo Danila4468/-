5138be5e13f9c083c75c9b02c8d8f7bb4b674ee7
D.A. Perevalov <mrsandvich4468@gmail.com>
2024-04-30 14:35:13 +0700

    Добавили второй пайп для передачи информации от ребёнка к родителю

--- pipe.c
+++ pipe.c
@@ -4,14 +4,19 @@
 #include <stdlib.h>
 int main(){
 // Создаём массив из двух элементов для хранения файловых дескрипторов для чтения и записи
-	int fd[2];
+	int fd1[2], fd2[2];
 	pid_t result;
 	size_t size;
-	char string[] = "Hello, world!";
-	char resstring[14];
-	if(pipe(fd)<0){
+	char string1[] = "Hello, child!", string2[] = "Hello, parent!";
+	char resstring1[13], resstring2[14];
+	if(pipe(fd1)<0){
 		printf("Can\'t create pipe\n");
-// Возврат признака аварийного завершения программы (-1)
+	// Возврат признака аварийного завершения программы (-1)
+		exit(-1);
+	}
+	if (pipe(fd2)<0){
+		printf("Can\'t create pipe\n");
+	// Возврат признака аварийного завершения программы (-1)
 		exit(-1);
 	}
 	result = fork();
@@ -20,39 +25,69 @@ int main(){
                 exit(-1);
 	}
 	else if (result>0) {
-		if (close(fd[0])<0) {
+		if (close(fd1[0])<0) {
 			printf("Ошибка при закрытии pipe на чтение\n");
 			exit(-1);
 		}
-		size = write(fd[1], string, 14);
-		// Проверка колличества байт, записанных в строку с помощью write()
-		if(size != 14){
-			printf("Ошибка при закрытии pipe на чтение\n");
+		if (close(fd2[1])<0) {
+                        printf("Ошибка при закрытии pipe на чтение\n");
+                        exit(-1);
+		}
+
+		size = write(fd1[1], string1, 13);
+		// Проверка колличества байт, записанных в строку
+		if(size != 13){
+			printf("Can\'t write all string\n");
 			exit(-1);
 		}
-		printf("Процесс родитель записал информацию в pipe и заканчивает работу \n");
-		if(close(fd[1])<0){
+		printf("Процесс родитель записал информацию в pipe\n");
+		size = read(fd2[0], resstring2, 14);
+		// Проверка колличества байт, записанных в строку
+		if(size != 14) {
+                        printf("Не получилось закрыть исходящий поток\n");
+                        exit(-1);
+                }
+		printf("Процесс родитель прочитал информацию из pipe и заканчивает работу: %s\n", resstring2);
+		if(close(fd1[1])<0){
 			printf("Не получилось закрыть исходящий поток\n");
 			exit(-1);
 		}
+		if(close(fd2[0])<0){
+                        printf("Не получилось закрыть исходящий поток\n");
+                        exit(-1);
+                }
 	}
 	else {
-		// Закрытие pipe для записи
-		if(close(fd[1])<0){
-			printf("Не получилось закрыть исходящий поток\n");
-			exit(-1);
-		}
-		size = read(fd[0], resstring, 14);
-		// Проверка количества байт, прочитанных из pipe() с помощью read()
- 		if(size != 14) {
-			printf("Не получилось закрыть исходящий поток\n");
-			exit(-1);
-		}
-		printf("%s\n", resstring);
-		// Закрытие pipe для чтения
-		if (close(fd[0]) < 0) {
+		if(close(fd1[1])<0){
 			printf("Не получилось закрыть входящий поток\n");
-		exit(-1);
+			exit(-1);
+		}
+		if(close(fd2[0])<0){
+                        printf("Не получилось закрыть исходящий поток\n");
+                        exit(-1);
+                }
+		size = read(fd1[0], resstring1, 13);
+		// Закрытие pipe для записи
+ 		if(size != 13) {
+			printf("Не прочитали строки\n");
+			exit(-1);
+		}
+		printf("Ребёнок прочитал строку: %s\n", resstring1);
+		size = write(fd2[1], string2, 14);
+		// Проверка колличество байт, прочитанных из pipe
+		if(size != 14) {
+                        printf("Не получилось вывести строки\n");
+                        exit(-1);
+                }
+		printf("Процесс ребёнка записал информацию в pipe \n");
+		// Проверка колличество байт, прочитанных из pipe
+		if (close(fd1[0]) < 0) {
+			printf("Не получилось закрыть входящий поток\n");
+			exit(-1);
+		}
+		if (close(fd2[1]) < 0) {
+                        printf("Не получилось закрыть исходящий поток\n");
+                	exit(-1);
 		}
 	}
 	return 0;
